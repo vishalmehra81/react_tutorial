3de456bdd360631e02ac5bc86dd908c73843ff64
Vishal Mehra <vishalmehra.81@gmail.com>
2021-02-25 01:42:57 +0000

    Simple Component

--- src/Table.js
+++ src/Table.js
@@ -1,40 +1,46 @@
-import React, { Component} from 'react';
+import React, {Component} from 'react'
 
+const TableHeader = () => {
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+    )
+}
+const TableBody = () => {
+    return (
+      <tbody>
+        <tr>
+          <td>Vishal</td>
+          <td>Unemployed</td>
+        </tr>
+        <tr>
+          <td>Nidhi</td>
+          <td>Java Developer</td>
+        </tr>
+        <tr>
+          <td>Shanaya</td>
+          <td>Student</td>
+        </tr>
+        <tr>
+          <td>Kabir</td>
+          <td>Student</td>
+        </tr>
+      </tbody>
+    )
+}
 class Table extends Component {
     render() {
-        return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Job</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Vishal</td>
-                        <td>Unemployed</td>
-                    </tr>
-                    <tr>
-                        <td>Nidhi</td>
-                        <td>Java Developer</td>
-                    </tr>
-                    <tr>
-                        <td>Shanaya</td>
-                        <td>Student</td>
-                    </tr>
-                    <tr>
-                        <td>Kabir</td>
-                        <td>Student</td>
-                    </tr>
-                    <tr>
-                        <td>Ravi</td>
-                        <td>Db Mgr</td>
-                    </tr>
-                </tbody>
-            </table>
-        )
+      return (
+        <table>
+          <TableHeader />
+          <TableBody />
+        </table>
+      )
     }
-}
+  }
 
-export default Table;
+  export default Table;
